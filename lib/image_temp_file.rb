058ab0e154e85c0b99709e41860331bb223a1d18
Sebastian Friedrich <sebastian@feldpost.com>
2010-04-29 05:31:27 -0500

    Ruby 1.9 Compat

--- lib/image_temp_file.rb
+++ lib/image_temp_file.rb
@@ -2,7 +2,7 @@ require "tempfile"
 
 module MiniMagick
   class ImageTempFile < Tempfile
-    def make_tmpname(ext, n)
+    def make_tmpname(ext, n=0)
       'mini_magick%d-%d%s' % [$$, n, ext ? ".#{ext}" : '']
     end
   end
