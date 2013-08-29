329aec5ff9a905b870bdfe778247dd32638861e1
Sebastian Friedrich <sebastian@feldpost.com>
2013-08-28 20:25:13 -0500

    Remove Image temp file

--- lib/image_temp_file.rb
+++
@@ -1,9 +0,0 @@
-require "tempfile"
-
-module MiniMagick
-  class ImageTempFile < Tempfile
-    def make_tmpname(ext, n)
-      'mini_magick%d-%d%s' % [$$, (n || 0), ext ? ".#{ext}" : '']
-    end
-  end
-end
